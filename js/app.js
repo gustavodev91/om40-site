8f18f1b617a232f479883ce35cb7079e2eed072e
gustavodev91 <gustavo.dev91@gmail.com>
2020-06-24 19:32:50 -0300

    menu fixo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
+
+var _containerMenu = $('[data-container="menu"]');
+
+console.log(_containerMenu);
+
+$(window).scroll(function(){
+  //if( $(this).scrollTop() > 73 ){
+    _containerMenu.addClass('menu-fixo');
+  //}else{
+    //_containerMenu.removeClass('menu-fixo');
+  //}
+})
+
 $(".custom-file-input").on("change", function() {
     var fileName = $(this).val().split("\\").pop();
     fileName = fileName.length > 20 ? fileName.substr(0,20)+'...' : fileName;
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
   });
+
+
