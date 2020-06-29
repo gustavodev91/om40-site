cb877ba44b3d20853d3090415c82b7e363a4a538
gustavodev91 <gustavo.dev91@gmail.com>
2020-06-29 16:48:40 -0300

    menu fixo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+
+
+var _containerMenu = $('[data-container="menu"]');
+
+var _divSuporte = $('[data-container="div-suporte"]');
+
+_divSuporte.css("height",_containerMenu.height());
+
+
 $(".custom-file-input").on("change", function() {
     var fileName = $(this).val().split("\\").pop();
     fileName = fileName.length > 20 ? fileName.substr(0,20)+'...' : fileName;
