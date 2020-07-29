db364a8dc1918b66f31662446469a3e812a8759a
gustavodev91 <gustavo.dev91@gmail.com>
2020-07-29 12:05:09 -0300

    correcao js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,18 +39,37 @@ $(function () {
                     mensagem : message
                 },
                 cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
+                success: function (e) {
+                    resp = JSON.parse(e);
+                    if(resp.msgEnviada){
+                        // Success message
+                        $("#success").html("<div class='alert alert-success'>");
+                        $("#success > .alert-success")
+                            .html(
+                                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                            )
+                            .append("</button>");
+                        $("#success > .alert-success").append(
+                            "<strong>Sua mensagem foi enviada. </strong>"
+                        );
+                        $("#success > .alert-success").append("</div>");
+                    }else{
+                        $("#success").html("<div class='alert alert-danger'>");
+                        $("#success > .alert-danger")
                         .html(
                             "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
                         )
                         .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Sua mensagem foi enviada. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
+                        $("#success > .alert-danger").append(
+                            $("<strong>").text(
+                                "Desculpe " +
+                                    firstName +
+                                    ", parece que meu servidor de email não está respondendo. Por favor, tente novamente mais tarde!"
+                            )
+                        );
+                        $("#success > .alert-danger").append("</div>");
+                    }
+
                     //clear all fields
                     $("#contactForm").trigger("reset");
                 },
